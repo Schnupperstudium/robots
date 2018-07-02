abd1c8244896763d252625c42032fd915e0c72c0
Simon Grossmann <simon.grossmann93@gmail.com>
2018-07-02 19:40:15 +0200

    Added helper methods to create EntityActions easily.

--- src/main/java/com/github/schnupperstudium/robots/ai/action/EntityAction.java
+++ src/main/java/com/github/schnupperstudium/robots/ai/action/EntityAction.java
@@ -1,6 +1,7 @@
 package com.github.schnupperstudium.robots.ai.action;
 
 import com.github.schnupperstudium.robots.entity.Entity;
+import com.github.schnupperstudium.robots.entity.Item;
 import com.github.schnupperstudium.robots.server.Game;
 
 public abstract class EntityAction {
@@ -9,5 +10,51 @@ public abstract class EntityAction {
 	@Override
 	public String toString() {
 		return getClass().getSimpleName();
+	}
+	
+	public static EntityAction noAction() {
+		return NoAction.INSTANCE;
+	}
+	
+	public static EntityAction dropItem(Item item) {
+		if (item == null)
+			return noAction();
+		
+		return dropItem(item.getUUID());
+	}
+	
+	public static EntityAction dropItem(long uuid) {
+		return new DropItemAction(uuid);
+	}
+	
+	public static EntityAction pickUpItem() {
+		return PickUpItemAction.INSTANCE;
+	}
+	
+	public static EntityAction useItem(Item item) {
+		if (item == null)
+			return noAction();
+		
+		return useItem(item.getUUID());
+	}
+	
+	public static EntityAction useItem(long uuid) {
+		return new UseItemAction(uuid);
+	}
+	
+	public static EntityAction moveForward() {
+		return MoveForwardAction.INSTANCE;
+	}
+	
+	public static EntityAction moveBackward() {
+		return MoveBackwardAction.INSTANCE;
+	}
+	
+	public static EntityAction turnLeft() {
+		return TurnLeftAction.INSTANCE;
+	}
+	
+	public static EntityAction turnRight() {
+		return TurnRightAction.INSTANCE;
 	}
 }
