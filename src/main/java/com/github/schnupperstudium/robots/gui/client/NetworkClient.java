f7206098ff3afb23bb397a3cae265dfe7228f810
Daniel Wieland <TheDwoon@users.noreply.github.com>
2019-08-28 22:19:23 +0200

    added names

--- src/main/java/com/github/schnupperstudium/robots/gui/client/NetworkClient.java
+++ src/main/java/com/github/schnupperstudium/robots/gui/client/NetworkClient.java
@@ -62,7 +62,7 @@ public class NetworkClient extends Application {
 			"30SecondsToWall", "IBims1Robot", "RobyWanKenoby", "LukeSkybotter", "RobaFett", "YouTouchedItLast",
 			"AllBotsAreBeautiful", "MichaelHackson", "GetTheFuckOffMyLAN", "CellarDweller", "JarJarBots", "AdmiralAckbot",
 			"Großbot Tarkin", "K-2SO", "AllBOTtledUp", "CookieBot", "GoodBot", "Who'sAGoodBot?", "BadBot", "BilboBotlin",
-			"Robottleneck", "2ez4me", "SudosPriest", "HotSingletonsInYourArea"
+			"Robottleneck", "2ez4me", "SudosPriest", "HotSingletonsInYourArea", "What'sUpBot?", "DasBot"
 	};
 	
 	private static final Logger LOG = LogManager.getLogger();
